68e860c7d3128470aa524b176a5a2b3517b28e73
Roter_Oktober <roter_oktober@roteroktober.(none)>
2011-06-16 22:11:58 +0200

    Network.cpp: implementation

--- stream/Network.cpp
+++ stream/Network.cpp
@@ -1,6 +1,8 @@
 #include "Network.h"
-
+#include "OperatorInterface.h"
 #include "OperatorNode.h"
+#include "OperatorWrapper.h"
+#include "Exception.h"
 
 namespace stream
 {
@@ -23,17 +25,86 @@ namespace stream
     
     void Network::activate()
     {
+	if (m_status == ACTIVE)
+	{
+	    throw InvalidStateException("Network already active");
+	}
+	
+	for(std::vector<OperatorNode*>::iterator iter = m_operators.begin();
+            iter != m_operators.end();
+            ++iter)
+        {
+            (*iter)->op()->activate();
+        }
+        
+	m_status = ACTIVE;
     }
 
     void Network::deactivate()
     {
+	for(std::vector<OperatorNode*>::iterator iter = m_operators.begin();
+            iter != m_operators.end();
+            ++iter)
+        {
+            (*iter)->op()->deactivate();
+        }
+        
+	m_status = INACTIVE;
     }
 
     OperatorNode*const Network::addOperator(Operator*const op)
     {
+	for(std::vector<OperatorNode*>::iterator iter = m_operators.begin();
+	    iter != m_operators.end();
+	    ++iter)
+	{
+	    if ((*iter)->op()->info() == reinterpret_cast<OperatorInfo*>(op))
+	    {
+	      throw InvalidStateException("Operator already exists");
+	      break;
+	    }
+	    
+	    OperatorWrapper* wrapper = new OperatorWrapper(op);
+            OperatorNode* node = new OperatorNode(wrapper);
+            m_operators.push_back(node);
+	    return node;
+	}
     }
 
     void Network::removeOperator(OperatorNode*const op)
     {
+	if (op == 0)
+	{
+	  throw ArgumentException("Invalid argument: Null pointer");
+	}
+	
+	for(std::vector<OperatorNode*>::iterator iter = m_operators.begin();
+	    iter != m_operators.end();
+	    ++iter)
+	{
+	    if ((*iter)->op() == reinterpret_cast<OperatorInterface*>(op))
+	    {
+	      m_operators.erase(iter);
+	      break;
+	    }
+	}
+	
+	throw ArgumentException("Operator does not exist");
+    }
+    
+    OperatorNode* const Network::getOperator(const std::string & name)
+    {
+	for(std::vector<OperatorNode*>::iterator iter = m_operators.begin();
+	    iter != m_operators.end();
+	    ++iter)
+	{
+	  if ((*iter)->name() == name)
+	  {
+	    return *iter;
+	  }
+	}  
+	
+	throw InvalidStateException("Operator does not exist");
+	return 0;
     }
 }
