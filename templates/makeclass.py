51fbac050313d1d34180044add1497d7e46a8a4e
Matthias Fuchs <matz.fuchs@gmx.at>
2011-06-23 15:11:56 +0200

    New version of makeclass.py. Temporary cmake files.

--- templates/makeclass.py
+++ templates/makeclass.py
@@ -3,6 +3,9 @@
 import datetime
 import string
 import re
+import getopt
+import sys
+
 
 #import argparse
 
@@ -11,6 +14,40 @@ import re
                     #help='File author')
 
 #args = parser.parse_args()
+
+def main():
+    author = ''
+    className = ''
+    package = ''
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "h:acp", ["help", "author=", "package=", "class="])
+    except getopt.GetoptError, err:
+        # print help information and exit:
+        print str(err) # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
+    output = None
+    verbose = False
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif o in ("-a", "--author"):
+            author = a
+        elif o in ("-p", "--package"):
+            package = a
+        elif o in ("-class", "--class"):
+            className = a
+        else:
+            assert False, "unhandled option"
+            usage()
+
+    process('Class.h.in', className + '.h', author, className, package)
+    process('Class.cpp.in', className + '.cpp', author, className, package)
+
+def usage():
+    print 'USAGE: python makeclass.py --author="Sean Connery" --package="stream" --class="Operator"'
 
 def replace(text, author, className, package):
     year = str(datetime.date.today().year)
@@ -24,10 +61,6 @@ def replace(text, author, className, package):
     text = re.sub(r'@YEAR@', year, text)
     text = re.sub(r'@HEADER@', header, text)
     return text
-    
-author = 'Matthias Fuchs'
-className = 'Operator'
-package = 'stream'
 
 def process(inFilePath, outFilePath, author, className, package):
     inFile = open(inFilePath, 'r')
@@ -38,5 +71,5 @@ def process(inFilePath, outFilePath, author, className, package):
     outFile.write(replace(text, author, className, package))
     outFile.close()
 
-process('Class.h.in', className + '.h', author, className, package)
-process('Class.cpp.in', className + '.cpp', author, className, package)
+if __name__ == "__main__":
+    main()
