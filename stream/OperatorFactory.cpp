c991378c652ef46adc8366d74733bb4514afa674
Roter_Oktober <roter_oktober@roteroktober.(none)>
2011-07-10 16:44:14 +0200

    StreamTest added

--- stream/OperatorFactory.cpp
+++ stream/OperatorFactory.cpp
@@ -1,4 +1,5 @@
 #include "OperatorFactory.h"
+#include "Exception.h"
 
 namespace stream
 {
@@ -9,7 +10,22 @@ namespace stream
 
     void OperatorFactory::registerOperator(const stream::Operator*const op)
     {
-
+        if(op == 0)
+        {
+            throw ArgumentException("Invalid argument: Null pointer");
+        }
+        
+        for(std::vector<Operator*>::iterator iter = m_operators.begin();
+            iter != m_operators.end();
+            ++iter)
+        {
+            if(op.name() == (*iter)->name() && op.package() == (*iter)->package())
+            {
+                throw ArgumentException("Invalid argument: Operator already registered");
+            }
+        }
+        
+        m_operators.push_back(op);
     }
 
     const std::vector< const stream::Operator* >& OperatorFactory::availableOperators() const
---
+++ stream/test/StreamTest.cpp
@@ -0,0 +1,60 @@
+/* 
+*  Copyright 2011 Thomas Fidler
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*/
+
+#include "StreamTest.h"
+#include <stream/Network.h>
+#include <stream/Stream.h>
+
+#include <cppunit/TestAssert.h> 
+#include <stream/Exception.h>
+
+CPPUNIT_TEST_SUITE_REGISTRATION (stream::StreamTest);
+
+namespace stream
+{
+    void StreamTest::setUp()
+    {
+        CPPUNIT_ASSERT_THROW(m_stream = new Stream(0),ArgumentException);
+        delete m_stream;
+        
+        m_network = new Network();
+        m_stream = new Stream(m_network);
+    }
+
+    void StreamTest::tearDown()
+    {
+        delete m_network;
+        delete m_stream;
+    }
+
+    void StreamTest::testNetwork()
+    {
+        CPPUNIT_ASSERT_EQUAL(m_stream->network(),m_network);
+        CPPUNIT_ASSERT(m_stream->network() != 0);
+    }
+//     
+//     void testAddThread()
+//     {
+//     }
+//     
+//     void testRemoveThread()
+//     {
+//     }
+//     
+//     void testThreads()
+//     {
+//     }
+}
