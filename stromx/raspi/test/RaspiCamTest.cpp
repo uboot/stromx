8b2a8a6551c2c227ad8b611c5d9e2ef4190f5710
Matthias Fuchs <matz.fuchs@gmx.at>
2014-07-09 20:55:57 +0200

    Add raspi cam test

---
+++ stromx/raspi/test/RaspiCamTest.cpp
@@ -0,0 +1,47 @@
+/* 
+*  Copyright 2014 Matthias Fuchs
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*/
+
+#include "stromx/raspi/test/RaspiCamTest.h"
+#include <cppunit/TestAssert.h>
+#include <cppunit/TestAssert.h>
+#include "stromx/raspi/ReadGpio.h"
+
+CPPUNIT_TEST_SUITE_REGISTRATION (stromx::raspi::RaspiCamTest);
+
+namespace stromx
+{
+    using namespace runtime;
+
+    namespace raspi
+    {
+        void RaspiCamTest::setUp()
+        {
+            m_operator = new OperatorTester(new ReadGpio());
+            m_operator->initialize();
+            m_operator->activate();
+        }
+
+        void RaspiCamTest::testExecute()
+        {
+            DataContainer data = m_operator->getOutputData(ReadGpio::OUTPUT);
+        }
+
+        void RaspiCamTest::tearDown()
+        {
+            delete m_operator;
+        }
+    }
+}
